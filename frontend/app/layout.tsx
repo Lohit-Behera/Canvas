abee00436036adf5d8048fc7de6076ef4afaa45e
Lohit <lohitsekhar.2002@gmail.com>
2024-12-13 17:20:46 +0530

    initial commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Product",
+  description: "Create a product and share it with the world",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="w-full min-h-screen flex flex-col justify-center items-center">
+          <main className="w-full flex-1 flex justify-center items-center my-6">
+            {children}
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+}
