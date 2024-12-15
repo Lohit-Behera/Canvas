122fa1b177a1951bfd340a7491d536c50946f0da
Lohit <lohitsekhar.2002@gmail.com>
2024-12-15 16:47:32 +0530

    fix Hydration problem with icon

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,23 +1,20 @@
 "use client";
 
-import { useTheme } from "next-themes";
-import { Button } from "@/components/ui/button";
-import { Moon, Sun } from "lucide-react";
+import dynamic from "next/dynamic";
+
+const ModeToggle = dynamic(
+  () => import("@/components/ModeToggle").then((mod) => mod.ModeToggle),
+  {
+    ssr: false,
+  }
+);
 
 function Header() {
-  const { theme, setTheme } = useTheme();
   return (
     <header className="z-20 w-full sticky top-0 p-2 backdrop-blur bg-background">
       <nav className="flex justify-between space-x-2">
         <h1 className="text-lg">Logo</h1>
-        <Button
-          className=""
-          variant="outline"
-          size="icon"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          {theme === "dark" ? <Sun /> : <Moon />}
-        </Button>
+        <ModeToggle />
       </nav>
     </header>
   );
