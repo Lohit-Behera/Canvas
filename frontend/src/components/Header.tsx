0076e4099b4ce0395ff58204acab559d4e10431c
Lohit <lohitsekhar.2002@gmail.com>
2024-12-22 18:07:35 +0530

    side bar added

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,6 +4,19 @@ import dynamic from "next/dynamic";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { useRouter, usePathname } from "next/navigation";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { ChevronDown, PanelLeft } from "lucide-react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 const ModeToggle = dynamic(
   () => import("@/components/ModeToggle").then((mod) => mod.ModeToggle),
   {
@@ -17,26 +30,102 @@ function Header() {
   return (
     <header className="z-20 w-full sticky top-0 p-2 backdrop-blur bg-background">
       <nav className="flex justify-between space-x-2">
-        <Link href="/">
-          <h1 className="text-lg">Logo</h1>
-        </Link>
-        <div className="flex space-x-2">
-          <Button
-            variant={pathname === "/create-product" ? "default" : "outline"}
-            size="sm"
-            onClick={() => router.push("/create-product")}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <PanelLeft />
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            side="left"
+            className="flex flex-col w-[200px] md:w-[300px]"
           >
-            Create Product
-          </Button>
-          <Button
-            variant={pathname === "/create-blog" ? "default" : "outline"}
-            size="sm"
-            onClick={() => router.push("/create-blog")}
-          >
-            Create Blog
-          </Button>
-          <ModeToggle />
-        </div>
+            <SheetHeader className="my-5">
+              <SheetTitle>
+                <Link href={"/"}>Logo</Link>
+              </SheetTitle>
+            </SheetHeader>
+            <Collapsible>
+              <CollapsibleTrigger
+                asChild
+                className="[&[data-state=open]>svg]:rotate-180"
+              >
+                <Button
+                  className="w-full flex justify-between"
+                  variant={"outline"}
+                  size="sm"
+                >
+                  Product
+                  <ChevronDown />
+                </Button>
+              </CollapsibleTrigger>
+              <CollapsibleContent className="flex justify-between my-2">
+                <span className="w-[1px] bg-muted-foreground/50 ml-2" />
+                <div className="w-[90%] flex flex-col justify-end space-y-2">
+                  <Button
+                    className="w-full flex justify-start"
+                    variant={
+                      pathname === "/create-product" ? "default" : "outline"
+                    }
+                    size="sm"
+                    onClick={() => router.push("/create-product")}
+                  >
+                    All Product
+                  </Button>
+                  <Button
+                    className="w-full flex justify-start"
+                    variant={pathname === "/product" ? "default" : "outline"}
+                    size="sm"
+                    onClick={() => router.push("/create-product")}
+                  >
+                    Add Product
+                  </Button>
+                </div>
+              </CollapsibleContent>
+            </Collapsible>
+            <Collapsible>
+              <CollapsibleTrigger
+                asChild
+                className="[&[data-state=open]>svg]:rotate-180"
+              >
+                <Button
+                  className="w-full flex justify-between"
+                  variant={"outline"}
+                  size="sm"
+                >
+                  Blog
+                  <ChevronDown />
+                </Button>
+              </CollapsibleTrigger>
+              <CollapsibleContent className="flex justify-between my-2">
+                <span className="w-[1px] bg-muted-foreground/50 ml-2" />
+                <div className="w-[90%] flex flex-col justify-end space-y-2">
+                  <Button
+                    className="w-full flex justify-start"
+                    variant={
+                      pathname === "/create-product" ? "default" : "outline"
+                    }
+                    size="sm"
+                    onClick={() => router.push("/create-product")}
+                  >
+                    All Product
+                  </Button>
+                  <Button
+                    className="flex justify-start"
+                    variant={
+                      pathname === "/create-blog" ? "default" : "outline"
+                    }
+                    size="sm"
+                    onClick={() => router.push("/create-blog")}
+                  >
+                    Add Blog
+                  </Button>
+                </div>
+              </CollapsibleContent>
+            </Collapsible>
+          </SheetContent>
+        </Sheet>
+        <ModeToggle />
       </nav>
     </header>
   );
